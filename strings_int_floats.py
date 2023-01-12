9cde1e9840796d75c12f301ba75c0d885de9bcf3
nerxxxy <nerxxxxy@gmail.com>
2023-01-12 10:19:51 +0200

    .

---
+++ strings_int_floats.py
@@ -0,0 +1,14 @@
+# string -> "Hello" , 'Hello', "Hello, 123" , "123!&/'}"
+
+print('Hello , how are you?')
+print('9')
+print('1.5')
+print("____")
+
+# integer -> 1,2,3,4,5
+
+print(9+1)
+
+# float -> 2.5 , 3.123
+
+print(1.5+1)
