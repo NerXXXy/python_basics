361d3d6c83a9290cc9c2d7c1e53771a2ec202411
nerxxxy <nerxxxxy@gmail.com>
2023-01-12 10:22:21 +0200

    how to convert data type

---
+++ data_type_conversion.py
@@ -0,0 +1,18 @@
+print(1+1)
+
+# int()
+print(int('1') + int('1'))
+
+# float()
+print(int('1') + float('1.9234'))
+
+#str
+
+a = 10
+b = 576
+
+print(str(a) + str(b))
+
+user_input = input('Type a number: ')
+
+print(int(user_input) + 5)
