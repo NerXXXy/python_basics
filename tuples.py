ec67a9648e1026d4b4e2765586d534ed155a6b01
nerxxxy <nerxxxxy@gmail.com>
2023-01-12 10:19:33 +0200

    tuples (like lists)

---
+++ tuples.py
@@ -0,0 +1,6 @@
+a = (23, 30.576, "Hello", [20, 'sup'], (23, 50))
+
+print(a)
+
+# pop , append and other functions are not usable for this type of lists with ()!
+# they do not mutate and information in them cant change without reasign
