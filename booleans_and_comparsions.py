25d9b5e46080cb2001f346415e2b16ca6225eb79
nerxxxy <nerxxxxy@gmail.com>
2023-01-12 10:23:06 +0200

    how boolean works

---
+++ booleans_and_comparsions.py
@@ -0,0 +1,13 @@
+print(2 != 2)
+print(2 == 2)
+print(2 <= 2)
+print(2 < 2)
+print(2 == 2.0)
+
+print('s' == 's')
+print('S' == 's')
+
+# depends to UTF format every character has its own number so if we compare strings -
+# their value depends on their number in UTF-8 list
+
+print('Volodymyr' > 'volodymyr')
