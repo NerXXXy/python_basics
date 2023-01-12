1e43490646aa9efd1b602aa997c5bdd440d37728
nerxxxy <nerxxxxy@gmail.com>
2023-01-12 10:18:51 +0200

    int,string,float

---
+++ variables.py
@@ -0,0 +1,17 @@
+name = "Vova"
+age = 67
+temperature = 36.6
+email_for_vova = 'example@gmail.com'
+
+print(name)
+print(age)
+print(temperature)
+print(email_for_vova)
+print('______')
+
+a = 3
+greeting = 'Hello,'
+name = 'Volodymyr'
+
+print(a)
+print(greeting + ' ' + name)
