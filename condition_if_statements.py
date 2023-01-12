bc5aa42981d0e54e6d0de1a6d3af5cc572927530
nerxxxy <nerxxxxy@gmail.com>
2023-01-12 10:22:36 +0200

    if elif else

---
+++ condition_if_statements.py
@@ -0,0 +1,11 @@
+a = 33
+number = 10
+
+if a < number:
+    print(f'{a} is less than 10 {number}')
+elif a > number:
+    print(f'{a} is bigger than {number}')
+elif a == number:
+    print(f'{a} is equal to 10 {number}')
+else:
+    print(f'error')
